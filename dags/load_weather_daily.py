9a8a7419a39ea4413572bc6d0dc86c14dd9805f2
DrShatsman <drshatsman@gmail.com>
2023-09-20 13:34:22 +0300

    Получение и сохранение данных через API с оркестратором Airflow

--- dags/load_weather_daily.py
+++ dags/load_weather_daily.py
@@ -4,15 +4,15 @@ from airflow.models import Variable
 from airflow.operators.bash import BashOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from datetime import datetime as dt
+from airflow.hooks.base_hook import BaseHook
 
 
-
-#connection = BaseHook.get_connection("miner_trg1")
+connection = BaseHook.get_connection(conn_id="postgres_local")
 
 default_args = {
     "owner": "etl_user",
     "depends_on_past": False,
-    "start_date": datetime(2023, 7, 17),
+    "start_date": datetime(2023, 9, 12),
     "retries": 1,
     "max_active_runs": 1
     #"retry_delay": timedelta(minutes=0.1)
@@ -23,9 +23,10 @@ dag = DAG('load_weather_daily', default_args=default_args, schedule_interval= '0
 
 
 
-clean_temp_table = BashOperator(
+clean_temp_table = PostgresOperator(
     task_id='clean_temp_table',
-    bash_command='python3 /airflow/scripts/load_weather_daily/clean_temp_table.py',
+    postgres_conn_id='postgres_local',
+    sql="""DELETE FROM Temperature_etl""",
     dag=dag)
 
 extract_data_and_load_into_temp_table = BashOperator(
@@ -38,14 +39,16 @@ check_data_in_the_temp_table = BashOperator(
     bash_command='python3 /airflow/scripts/load_weather_daily/check_data_in_the_temp_table.py',
     dag=dag)
 
-load_data_into_table = BashOperator(
+load_data_into_table = PostgresOperator(
     task_id='load_data_into_table',
-    bash_command='python3 /airflow/scripts/load_weather_daily/load_data_into_table.py',
+    postgres_conn_id='postgres_local',
+    sql="""INSERT INTO Temperature SELECT * FROM Temperature_etl""",
     dag=dag)
 
-again_clean_temp_table = BashOperator(
+again_clean_temp_table = PostgresOperator(
     task_id='again_clean_temp_table',
-    bash_command='python3 /airflow/scripts/load_weather_daily/again_clean_temp_table.py',
+    postgres_conn_id='postgres_local',
+    sql="""DELETE FROM Temperature_etl""",
     dag=dag)
 
 
